9e765d16a1caf5f441406aeb9a85cec0239a01b4
MoinulIslam7 <moinul.islam@gigalogy.com>
2025-05-18 11:14:34 +0600

    update

--- src/views/DashboardC.vue
+++ src/views/DashboardC.vue
@@ -23,6 +23,48 @@ export default {
         }
     },
     created() {
+        this.$store.commit('SET_LOADING', true);
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const coords = `${position.coords.latitude},${position.coords.longitude}`;
+                this.$store.dispatch('changeCity', coords);
+            },
+            () => {
+                this.$store.dispatch('fetchWeather');
+            }
+        );
+    }
+
+};
+
+</script>
+
+<!-- 
+<template>
+    <v-container>
+        <CitySelector />
+        <v-progress-circular v-if="loading" indeterminate />
+        <div v-else>
+            <WeatherCard />
+            <h4 class="my-4">3-Day Forecast</h4>
+            <ForecastCard />
+        </div>
+    </v-container>
+</template>
+
+<script>
+import WeatherCard from '@/components/WeatherCard.vue';
+import ForecastCard from '@/components/ForecastCard.vue';
+import CitySelector from '@/components/CitySelector.vue';
+
+export default {
+    components: { WeatherCard, ForecastCard, CitySelector },
+    computed: {
+        loading() {
+            return this.$store.getters.isLoading;
+        }
+    },
+    created() {
         this.$store.dispatch('fetchWeather');
 
         if (!this.$store.state.selectedCity) {
@@ -34,4 +76,4 @@ export default {
     }
 };
 
-</script>
+</script> -->
